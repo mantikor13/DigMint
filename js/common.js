c87ee56e4f55808d01ef5389cc508a4321129e6d
mantikor13 <romakrut333bk@gmail.com>
2017-10-31 12:01:51 +0200

    fix

--- js/common.js
+++ js/common.js
@@ -189,7 +189,9 @@ let modalConsistCounter = 0;
         });
     };
 
-(function modalLabelMove(){
+(function modalForm(){
+
+    //---Lable move---//
 
     $(".contact-form__input, .contact-form__textarea").click(function(event){
 
@@ -206,6 +208,33 @@ let modalConsistCounter = 0;
 
     });
 
+    //---Scroll on button---//
+
+        $(document).ready(function(){
+
+            $(".scrollLink").on("click","a", function (event) {
+
+      //отменяем стандартную обработку нажатия по ссылке
+
+            event.preventDefault();
+
+      //забираем идентификатор бока с атрибута href
+
+            var id  = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+
+            $('body,html').animate({scrollTop: top}, 1500);
+
+            });
+
+        });
+
+
 })();
 
 (function slider(){
@@ -217,8 +246,8 @@ let modalConsistCounter = 0;
     let controlLeft = $(".viewControlLeft");
     let controlRight = $(".viewControlRight");
 
-    let iconsWidth = icons.css("width");
-    icons.css("height", iconsWidth);
+    //let iconsWidth = icons.css("width");
+    //icons.css("height", iconsWidth);
 
     iconsImg.click(function(event){
 
