d5603d150ac76e65a51929ad49166e178c686a70
mantikor13 <romakrut333bk@gmail.com>
2017-11-27 20:50:21 +0200

    burger

--- js/common.js
+++ js/common.js
@@ -132,7 +132,7 @@ function initSmoothTransition() {
 
         var t = 1000;
         var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-        $('html,body').stop().animate({scrollTop: $(d).offset().top - 100}, t);
+        $('html,body').stop().animate({scrollTop: $(d).offset().top}, t);
     });
 };
 
